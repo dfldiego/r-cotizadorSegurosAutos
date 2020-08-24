2cc208a5dd5245a32e1ab713a915085a5ffc9c5d
dfldiego <dfl.diego92@gmail.com>
2020-08-24 15:20:21 -0300

    agregando state al formulario. leyendo informacion de lo que el usuario selecciona.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,53 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
+
+const Campo = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;   /**quita la apariencia por defecto del select y nos habilita a aplicar css */
+`;
+
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+    background-color: #00838f;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover {
+        background-color: #26c6da;
+        cursor: pointer;
+    }
+`;
 
 const Formulario = () => {
 
-    const Campo = styled.div`
-        display: flex;
-        margin-bottom: 1rem;
-        align-items: center;
-    `;
+    const [datos, setDatos] = useState({
+        marca: '',
+        year: '',
+        plan: ''
+    });
 
-    const Label = styled.label`
-        flex: 0 0 100px;
-    `;
+    //Extraer los datos del state.
+    const { marca, year, plan } = datos;
 
-    const Select = styled.select`
-        display: block;
-        width: 100%;
-        padding: 1rem;
-        border: 1px solid #e1e1e1;
-        -webkit-appearance: none;   /**quita la apariencia por defecto del select y nos habilita a aplicar css */
-    `;
-
-    const InputRadio = styled.input`
-        margin: 0 1rem;
-    `;
-
-    const Boton = styled.button`
-        background-color: #00838f;
-        font-size: 16px;
-        width: 100%;
-        padding: 1rem;
-        color: #fff;
-        text-transform: uppercase;
-        font-weight: bold;
-        border: none;
-        transition: background-color .3s ease;
-        margin-top: 2rem;
-
-        &:hover {
-            background-color: #26c6da;
-            cursor: pointer;
-        }
-    `;
+    //leer los datos del formulario y colocarlo en el state
+    const obtenerInformacion = e => {
+        setDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        })
+    }
 
     return (
         <form>
             <Campo>
                 <Label>Marca</Label>
-                <Select>
+                <Select
+                    name="marca"
+                    value={marca}
+                    onChange={obtenerInformacion}
+                >
                     <option value="">-- Seleccione --</option>
                     <option value="americano">Americano</option>
                     <option value="europeo">Europeo</option>
@@ -56,7 +77,11 @@ const Formulario = () => {
             </Campo>
             <Campo>
                 <Label>Año</Label>
-                <Select>
+                <Select
+                    name="year"
+                    value={year}
+                    onChange={obtenerInformacion}
+                >
                     <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -76,11 +101,15 @@ const Formulario = () => {
                     type="radio"
                     name="plan"
                     value="basico"
+                    checked={plan === "basico"}
+                    onChange={obtenerInformacion}
                 /> Básico
                 <InputRadio
                     type="radio"
                     name="plan"
                     value="completo"
+                    checked={plan === "completo"}
+                    onChange={obtenerInformacion}
                 /> Completo
             </Campo>
             <Boton type="button">Cotizar</Boton>
