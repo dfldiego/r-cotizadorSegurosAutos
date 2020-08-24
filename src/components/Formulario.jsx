cf4a4adfc4f29979d6bf37f5dbbef7bc6b73e7ea
dfldiego <dfl.diego92@gmail.com>
2020-08-24 14:04:40 -0300

    styled component del formulario: finalizado. styled: boton.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -25,6 +25,24 @@ const Formulario = () => {
         margin: 0 1rem;
     `;
 
+    const Boton = styled.button`
+        background-color: #00838f;
+        font-size: 16px;
+        width: 100%;
+        padding: 1rem;
+        color: #fff;
+        text-transform: uppercase;
+        font-weight: bold;
+        border: none;
+        transition: background-color .3s ease;
+        margin-top: 2rem;
+
+        &:hover {
+            background-color: #26c6da;
+            cursor: pointer;
+        }
+    `;
+
     return (
         <form>
             <Campo>
@@ -65,7 +83,7 @@ const Formulario = () => {
                     value="completo"
                 /> Completo
             </Campo>
-            <button type="button">Cotizar</button>
+            <Boton type="button">Cotizar</Boton>
         </form>
     );
 }
